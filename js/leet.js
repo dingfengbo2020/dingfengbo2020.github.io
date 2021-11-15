665377b8f891540885544c6a24ce21b278e2ac29
x-aowei2021 <kevinxaw456@163.com>
2021-11-15 23:49:41 +0800

    Site updated: 2021-11-15 23:49:41

--- js/leet.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Zephyr');
-});
